b5dbd8b4de1e68c495435d6be597e9a2623da07f
PrabuSankarBose <prabusankarbose@gmail.com>
2024-02-02 16:12:50 +0530

    more files added

--- js/student.js
+++ js/student.js
@@ -16,4 +16,34 @@ function sign_up()
     document.getElementById("sign-in-button").style.color = "azure";
     document.getElementById("sign-up-button").style.backgroundColor = "azure";
     document.getElementById("sign-up-button").style.color = "hotpink";
+    document.getElementById("roll-no").attributes="autofocus";
 }
+
+
+
+// function signin_validation()
+// {
+//     let username = documnet.forms["signin_form"]["user-name"].value;
+//     let password = documnet.forms["signin_form"]["pwd"].value;
+//     if ( username == "" || password == "" ) 
+//     {
+//         alert("No fields must be empty");
+//         return false;
+//     }
+
+// } 
+// function signup_validation()
+// {
+//     let rollno = documnet.forms["signup_form"]["roll-no"].value;
+//     let username = documnet.forms["signup_form"]["user-name"].value;
+//     let password = documnet.forms["signup_form"]["pwd"].value;
+//     let address = documnet.forms["signup_form"]["address"].value;
+//     let phone_no = documnet.forms["signup_form"]["phone-no"].value;
+//     let cpassword = documnet.forms["signup_form"]["cpwd"].value;
+//     if ( username == "" || password == "" ) 
+//     {
+//         alert("No fields must be empty");
+//         return false;
+//     }
+
+// } 
--- src/result.php
+++ src/result.php
@@ -7,7 +7,40 @@
     <link rel="stylesheet" href="../css/result.css">
 </head>
 <body>
+    <div id="notification">
+        <div>LATEST NEWS</div>
+        <marquee>Latest notification: Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, sapiente!</marquee>
+        
+    </div>
+    <div id="header"><a href="../index.html">
+        <img id="logo" src="../img/enterance.jpg" alt="enterance image"></a>
+        <h1 id="title">K.C.A.D Chidambaram Gnangiri Matric Hr.Sec School</h1>
+    </div>
+    
+    <div id="nav_bar">
+        <ul>
+            <li><a href="../index.html">Home</a></li>
+            <li><a href="gallery.html">Gallery</a></li>
+            <li><a href="admission.php">Admission</a></li>
+            <li><a href="student.php">Student's Portal</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">About us</a></li>
+        </ul>
+    </div>
    <div>
+   <?php
+        $conn = new mysqli("localhost","root","","KCAD");
+        if ($conn->connect_error) 
+        {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        else
+        {
+            echo "Connection successfull";
+        }
+
+    ?>
+
        Welcome Your email address is: <?php echo $_POST["email"]; ?><br>
        Your Password  is: <?php echo $_POST["pwd"]; ?>
        <br>
