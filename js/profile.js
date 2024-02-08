cb6e095d88f1fa4551a4497db27a3c1ea45f4317
PrabuSankarBose <prabusankarbose@gmail.com>
2024-02-08 13:33:10 +0530

    some files are updated

---
+++ js/profile.js
@@ -0,0 +1,22 @@
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() 
+{
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) 
+  { 
+    
+    
+    document.getElementById("header").style.height = "75px;";
+    document.getElementById("logo").style.height = "50px";
+    document.getElementById("logo").style.width = "50px";
+    
+    
+  } 
+  else 
+  {
+    document.getElementById("header").style.height = "auto";
+    document.getElementById("logo").style.height = "150px";
+    document.getElementById("logo").style.width = "150px";
+    
+    
+  }
+}
--- src/profile.php
+++ src/profile.php
@@ -13,7 +13,7 @@
         
     </div>
     <div id="header"><a href="../index.html">
-        <img id="logo" src="../img/enterance.jpg" alt="enterance image"></a>
+        <img id="logo" src="../img/badge_black.png" alt="enterance image"></a>
         <h1 id="title">K.C.A.D Chidambaram Gnangiri Matric Hr.Sec School</h1>
     </div>
     
@@ -28,71 +28,134 @@
         </ul>
     </div>
     </div>
-    <div id="studprofile">
-        <div id="sidebar">
-            <ul>
-                <li>Profile</li>
-                <li>Fees Structure </li>
-                <li>Materials</li>
-                <li>Recorded Classes</li>
-                <li>Test Results</li>
-                <li>Class Teacher</li>
-            </ul>
-
-        </div>
-        <div id="details">
-            <div>
-                <img src="../img/userimg.jpeg" alt="profile pic" id="profpic">
-            </div>
-
-
-
-
-            
+                
    <div>
-       Welcome Your email address is: <?php echo $_POST["roll-no-signin"]; ?><br>
-       Your Password  is: <?php echo $_POST["pwd-signin"]; ?>
-       <br>
-    <?php
-        $conn = new mysqli("localhost","root","","KCAD");
-        if ($conn->connect_error) 
-        {
-            die("Connection failed: " . $conn->connect_error);
-        }
-        else
-        {
-            echo "Connection successfull";
-        }
-        $sql = "SELECT rollno,pass_word FROM student";
-        $result = $conn->query($sql);
-        if (mysqli_num_rows($result) > 0) 
-        {
-            // output data of each row
-            while($row = mysqli_fetch_assoc($result)) 
+       <?php
+            try
             {
-                if($row["rollno"] == $_POST["roll-no-signin"] and $row["pass_word"] == $_POST["pwd-signin"])
+                $conn = new mysqli("localhost","root","","KCAD");
+                if ($conn->connect_error) 
                 {
-                    echo "Roll-no: " . $row["rollno"]. " -Password: " . $row["pass_word"]. " " ."<br>";
+                    die("Connection failed: " . $conn->connect_error);
                 }
-                else
+
+                $sql = "SELECT * FROM student";
+                $sq2 = "SELECT * FROM ranklist";
+                $result1 = $conn->query($sql);
+                $result2 = $conn->query($sq2);
+                if (mysqli_num_rows($result1) > 0) 
                 {
-                    echo "Roll-no: " . $_POST["roll-no-signin"]. " -Password: " . $_POST["pwd-signin"]. " is wrong " ."<br>";
+                    // output data of each row
+                    while($row = mysqli_fetch_assoc($result1)) 
+                    {
+                        if($row["rollno"] == $_POST["roll-no-signin"] and $row["pass_word"] == $_POST["pwd-signin"])
+                        {
+
+                            $rollno = $row["rollno"];
+                            $name = $row["name"];
+                            $gender = $row["gender"];
+                            $email = $row["email"];
+                            $address = $row["address"];
+                            $dob = $row["dateofbirth"];
+                            $phone_no = $row["phoneno"];
+                            $standard = $row["standard"];
+                            while($row1 = mysqli_fetch_assoc($result2)) 
+                            {
+                                if($row1["rollno"] == $rollno)
+                                {
+
+                                    $english = $row1["english"];
+                                    $language = $row1["language"];
+                                    $maths = $row1["maths"];
+                                    $science = $row1["science"];
+                                    $social = $row1["social"];
+                                    $total = $row1["total"];
+                                    $rank = $row1["rank"];
+                                
+                                }
+                            
+                                else
+                                {
+                                    echo "Roll-no: " . $_POST["roll-no-signin"]. " -total: " . $_POST["pwd-signin"]. " is wrong " ."<br>" ."in mark section";
+                                }
+
+                            }
+
+                        }
+
+                        else
+                        {
+                            echo "Roll-no: " . $_POST["roll-no-signin"]. " -Password: " . $_POST["pwd-signin"]. " is wrong " ."<br>"."in main section";
+                        }
+
+                    }
+                }
+                else 
+                {
+                    echo "0 results";
                 }
             }
-        }
-        else 
-        {
-            echo "0 results";
-        }
-        
-        $conn->close();
-        $demo=phpversion();
-        echo $demo;
-        
-    ?>
+            catch(Exception $e) 
+            {
+                echo $e;
+            }
+        ?>
 
+
+        <div id="studprofile">
+            <div id="sidebar">
+                <ul>
+                    <li>Profile</li>
+                    <li>Fees Structure </li>
+                    <li>Materials</li>
+                    <li>Recorded Classes</li>
+                    <li>Test Results</li>
+                    <li>Class Teacher</li>
+                    <li>Class Teacher</li>
+                </ul>
+
+            </div>
+            <div id="details">
+                <div id="profile-div">
+                    <div id="profile-1">
+                        <image>
+                            <img src="../img/userimg.jpeg" alt="profile pic" id="profpic">
+                        </image>
+                        <div>
+                            <label for="name">Name</label>
+                            <text><?php echo $name; ?></text>
+                        </div>
+                        <div>
+                            <label for="roll-number">Roll-No</label>
+                            <text><?php echo $rollno; ?></text>
+                        </div>
+                        <div>
+                            <label for="standard">Standard</label>
+                            <text><?php echo $standard," <sup>th</sup>"; ?></text>
+                        </div>
+                    </div>
+                    <div id="profile-2">
+                        <h3><?php echo $email; ?></h3>
+                        <h3><?php echo $phone_no; ?></h3>
+                        <h3><?php echo $address; ?></h3>
+                        <h3><?php echo $dob; ?></h3>
+                        <h3><?php echo $gender; ?></h3>
+                        <h3><?php echo $english; ?></h3>
+
+                    </div>
+                </div>
+                <div id="test_result">
+
+                </div>
+            </div>
         </div>
-    </div>  
-   
+
+
+        <?php
+        $conn->close();
+        ?>
+    </div>
+    
+   <script src="../js/profile.js"></script>
 </body>
 </html>
